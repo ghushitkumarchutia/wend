eec9003cfc37127e703cd23291ce0d97fd5e5149
Ghushit Kumar Chutia <ghushit45@gmail.com>
2026-07-03 14:40:20 +0530

    chore(root): configure linting and formatting rules

---
+++ .eslintrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  extends: ['eslint:recommended'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+};
